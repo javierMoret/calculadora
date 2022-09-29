a770b76fa86ac795fa4636373190b9d2d3c21011
Javier Moret Galán <javier.s257989@cesurformacion.com>
2022-09-29 14:00:21 +0200

    Calculadora

---
+++ app.js
@@ -0,0 +1,40 @@
+
+function calcular(){
+
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var radios = document.getElementsByName("operadores");
+    for (var radio of radios)
+    {
+        if (radio.checked) {
+            console.log(radio.value);
+            var operadorSeleccionado = parseInt(radio.value);
+        }
+    }
+    console.log(operadorSeleccionado);
+    var resultado;
+
+        switch(operadorSeleccionado){
+            case 0:
+                 resultado=n1+n2;
+                 break;
+            case 1:
+                 resultado=n1-n2;
+                 break;
+            case 2:
+                 resultado=n1*n2;
+                 break;
+            case 3:
+                 resultado=n1/n2;
+                 break;
+            case 4:
+                 resultado=n1%n2;
+                 break;
+            default:
+                resultado="Sin operador"
+        }
+    document.getElementById("resultado").innerHTML=resultado;
+
+    
+
+}
